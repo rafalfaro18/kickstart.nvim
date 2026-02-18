56ec6b7d6eb38fbdd42a8e7b07d998fe82dd7297
Cachy <16846380+rafalfaro18@users.noreply.github.com>
2026-02-17 20:30:33 -0600

    configure indent blankline to be rainbow colored

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,11 +5,42 @@
 return {
 
   {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      ---@module "ibl"
-      ---@type ibl.config
-      opts = {},
-  }
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = function(_, opts)
+      -- We'll inject rainbow colors below
+      opts.indent = opts.indent or {}
+      return opts
+    end,
+    config = function(_, opts)
+      -- Register rainbow highlights before setup
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        -- Define your rainbow color palette
+        vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+      end)
+
+      -- Enable rainbow indent highlights
+      opts.indent.highlight = highlight
+
+      require("ibl").setup(opts)
+    end,
+  }, 
 
 }
