19666da86516c0c51868d136aae4d81141016660
Cachy <16846380+rafalfaro18@users.noreply.github.com>
2026-02-17 21:20:46 -0600

    comment ts_ls due to error

--- init.lua
+++ init.lua
@@ -602,7 +602,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
