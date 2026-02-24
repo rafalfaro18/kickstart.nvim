b2678854bfdc95d8ddc69cf5d7362505fba62629
Rafael Alfaro <rafalfaro_18@outlook.com>
2026-02-24 14:55:56 -0600

    Configure rounded border for which-key options
    Add rounded border option for which-key plugin.

--- init.lua
+++ init.lua
@@ -308,6 +308,9 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter',
     opts = {
+      win = {
+        border = "rounded",
+      },
       -- delay between pressing a key and opening which-key (milliseconds)
       delay = 0,
       icons = { mappings = vim.g.have_nerd_font },
