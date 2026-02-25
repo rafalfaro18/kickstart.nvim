9fea71b2ec364a03611295a0bc808c22d38c5eb4
Rafael Alfaro <rafalfaro_18@outlook.com>
2026-02-25 15:49:30 -0600

    Add keymap for saving files with leader+w

--- init.lua
+++ init.lua
@@ -229,6 +229,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save file' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
