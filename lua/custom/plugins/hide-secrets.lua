fac001f0a734289d8295bc0d0109e9af0b626bd9
Rafael Alfaro <rafalfaro_18@outlook.com>
2026-05-20 15:46:17 -0600

    Enhance veil.nvim setup with extra patterns
    Added extra patterns for hiding secrets in files.

--- lua/custom/plugins/hide-secrets.lua
+++ lua/custom/plugins/hide-secrets.lua
@@ -1,4 +1,10 @@
 -- Hides secrets in files like .env
 vim.pack.add { 'https://github.com/Gentleman-Programming/veil.nvim' }
 
-require("veil").setup()
+require("veil").setup({
+  -- Add extra patterns - simple keywords or full patterns
+  extra_patterns = {
+    "POSTGRES_URL",      -- Simple keyword
+    "NEXT_PUBLIC_SUPABASE_URL",    -- Another keyword
+  },
+})
