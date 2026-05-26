33f3cc71aba46fd8cd1cf87f126b688612afbaca
Rafael Alfaro <rafalfaro_18@outlook.com>
2026-05-26 13:48:50 -0600

    Add open mapping for toggleterm in lazygitfloat

--- lua/custom/plugins/lazygitfloat.lua
+++ lua/custom/plugins/lazygitfloat.lua
@@ -2,6 +2,7 @@ vim.pack.add { 'https://github.com/akinsho/toggleterm.nvim' }
 
 require("toggleterm").setup {
   direction = "float",
+  open_mapping = [[<c-\>]],
   float_opts = {
     border = "rounded",
   }
