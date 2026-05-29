09f908f018b6335bcebc980388ecd5032b73d8fe
Rafael Alfaro <rafalfaro_18@outlook.com>
2026-05-29 16:12:45 -0600

    Add keymaps for saving all and LSP code actions

--- lua/custom/plugins/mykeymaps.lua
+++ lua/custom/plugins/mykeymaps.lua
@@ -3,3 +3,14 @@ vim.keymap.set('n', 'K', function()
 end, { desc = "Hover Documentation" })
 
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>wa', '<cmd>wa<CR>', { desc = 'Write all modified buffers' })
+
+vim.keymap.set('n', '<leader>mf', function()
+  vim.lsp.buf.code_action({
+    context = {
+      diagnostics = {},
+      only = { 'refactor.move' }
+    },
+    apply = true,
+  })
+end, { desc = "LSP Move to new file" })
