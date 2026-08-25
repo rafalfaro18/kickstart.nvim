d0010fbbccf5fee60e0f364db6d2ccd96a2f7957
work <work@example.com>
2026-08-25 15:52:06 -0600

    hides also in markdown files & hides ghp_*

--- lua/custom/plugins/hide-secrets.lua
+++ lua/custom/plugins/hide-secrets.lua
@@ -2,9 +2,13 @@
 vim.pack.add { 'https://github.com/Gentleman-Programming/veil.nvim' }
 
 require("veil").setup({
+  extra_files = {
+    "*.md",
+  },
   -- Add extra patterns - simple keywords or full patterns
   extra_patterns = {
     "POSTGRES_URL",      -- Simple keyword
     "NEXT_PUBLIC_SUPABASE_URL",    -- Another keyword
+    { pattern = "(ghp_)([A-z%d]*)", group = 2 }
   },
 })
