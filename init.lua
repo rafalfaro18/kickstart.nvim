eb61e22f4e35783734d56a5947d28c56b230d797
Work <work@example.com>
2026-09-07 16:51:41 -0600

    re-enable autopairs and neo-tree

--- init.lua
+++ init.lua
@@ -1040,8 +1040,8 @@ do
   require 'kickstart.plugins.debug'
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
-  -- require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.neo-tree'
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
   --
